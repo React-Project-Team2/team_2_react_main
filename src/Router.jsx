fd6943c555e0f1d0884315f7c655cedd04beb2d8
NEMURENAIYORU <lhslhs301@gmail.com>
2023-11-02 19:42:18 +0900

    ✨ feat : page라우팅

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage'
+import AboutPage from './pages/AboutPage'
+import BoardPage from './pages/BoardPage'
+import ForumPage from './pages/ForumPage'
 
 const Router = () => {
   return (
     <Routes>
       <Route index element={<MainPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/board" element={<BoardPage />} />
+      <Route path="/forum" element={<ForumPage />} />
     </Routes>
   )
 }
