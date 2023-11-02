5fc58ffc75cf8ea7ea1486a4b542e7e182a3da13
NEMURENAIYORU <lhslhs301@gmail.com>
2023-11-02 11:40:54 +0900

    🎉 init : Start Project

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">Hello World!</button>
+    </div>
+    
+  )
+}
+
+export default MainPage
