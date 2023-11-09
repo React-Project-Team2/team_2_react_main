b708396f94701d59a4f6aa7b25f30bbe04eec6d2
NEMURENAIYORU <lhslhs301@gmail.com>
2023-11-09 19:48:02 +0900

    ✨ feat : 로그인, 회원가입페이지 라우팅

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import AboutPage from './pages/AboutPage'
 import BoardPage from './pages/BoardPage'
+import SignUpPage from './pages/SignUpPage'
+import SignInPage from './pages/SignInPage'
 
 const Router = () => {
   return (
@@ -10,6 +12,8 @@ const Router = () => {
       <Route index element={<MainPage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/board" element={<BoardPage />} />
+      <Route path="/signUp" element={<SignUpPage />} />
+      <Route path="/signIn" element={<SignInPage />} />
     </Routes>
   )
 }
