4eff4eb96f8957eb624df65c456bbaf69671bfd0
tngjs3327 <psheon9714@gmail.com>
2023-11-10 12:49:49 +0900

    1110 작업

--- src/pages/BoardInsert.jsx
+++ src/pages/BoardInsert.jsx
@@ -1,37 +1,43 @@
 import React from 'react';
-import Form from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 
 const BoardInsert = () => {
   return (
-    <div className='container-md' id='board-insert-div'>
-      <Form >
+    <>
+      <div className='container-md board-insert-div' id='board-insert-div'>
+        <Form >
 
-        <Form.Select aria-label="Default select example">
-          <option value="no">카테고리</option>
-          <option value="후쿠오카">후쿠오카</option>
-          <option value="나가사키">나가사키</option>
-          <option value="구마모토">구마모토</option>
-          <option value="유후인">유후인</option>
-          <option value="사가">사가</option>
-        </Form.Select>
+          <Form.Group className='mb-3'>
+            <Form.Label>카테고리</Form.Label>
+            <Form.Select  >
+              <option value="">선택하세요</option>
+              <option value="후쿠오카">후쿠오카</option>
+              <option value="나가사키">나가사키</option>
+              <option value="구마모토">구마모토</option>
+              <option value="유후인">유후인</option>
+              <option value="사가">사가</option>
+            </Form.Select>
+          </Form.Group>
+          
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor='nickName'>닉네임</Form.Label>
+            <Form.Control type="text" name='nickName' id='nickName' value="닉네임" readOnly  />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>닉네임</Form.Label>
-          <Form.Control type="nickName" defaultValue="닉네임" readOnly />
-        </Form.Group>
+          <Form.Group className="mb-3" >
+            <Form.Label htmlFor='title'>제목</Form.Label>
+            <Form.Control type='text' name="title" id='title' placeholder="제목을 입력하세요" />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>제목</Form.Label>
-          <Form.Control type="title" placeholder="제목을 입력하세요" />
-        </Form.Group>
+          <Form.Group className="mb-3" >
+            <Form.Label>내용</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
-        </Form.Group>
+        </Form>
+      </div>
 
-      </Form>
-    </div>
+    </>
   );
 }
 
