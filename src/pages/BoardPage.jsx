04d2ca4eb1eb5030e096d63adf09f4e3dc814503
jk <yjkeong@gmail.com>
2023-11-19 15:14:30 +0900

    ✨ feat : 메인 페이지와 동일하게 게시판 양식 추가

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../styles/BoardPage.css';
+import ContainerNavbar from '../components/common/containNavbar/ContainerNav.jsx';
 
 const BoardPage = () => {
   return (
-    <div>BoardPage</div>
+    <>
+      <div className='container-fluid board'>
+        <div className='w-auto'>
+          <div className="pb-5 pt-5 container text-center">
+            <h1>현지 학기제</h1>
+            <h3>sub name</h3>
+          </div>
+          <div className="bg-white container p-0">
+            <ContainerNavbar />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
