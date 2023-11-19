033b7779e6ac5ebb6796f54bb8868349e964acf6
jk <yjkeong@gmail.com>
2023-11-19 16:56:07 +0900

    ✨ feat : 게시글 작성 폼과 연결

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import '../styles/BoardPage.css';
 import ContainerNavbar from '../components/common/containNavbar/ContainerNav.jsx';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const BoardPage = () => {
+  const navigate = useNavigate();
+
+  const handleButtonClicked = () => {
+    navigate('/board/create');
+  };
+
   return (
     <>
       <div className='container-fluid board'>
@@ -13,7 +21,19 @@ const BoardPage = () => {
           </div>
           <div className="bg-white container p-0">
             <ContainerNavbar />
-            <img src="https://www.his-j.com/kokunai/kanto/tour_info/okinawa/catchy/wp-content/uploads/2021/02/post_10242_01-730x410.jpg" className="img-ht img-fluid rounded" alt=''/>
+            <img src="https://www.his-j.com/kokunai/kanto/tour_info/okinawa/catchy/wp-content/uploads/2021/02/post_10242_01-730x410.jpg" className="img-ht img-fluid rounded" alt='' />
+            <Container className='my-3 ms-4'>
+              <Row>
+                <Col xs={2} className='border-end text-center'>
+                  <h4>지역</h4>
+                </Col>
+                <Col xs={7}>
+                </Col>
+                <Col xs={3}>
+                  <Button variant="secondary" onClick={handleButtonClicked}>게시글 작성</Button>
+                </Col>
+              </Row>
+            </Container>
           </div>
         </div>
       </div>
