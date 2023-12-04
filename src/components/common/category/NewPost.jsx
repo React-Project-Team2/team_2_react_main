a05246ef1e6f88332097d621823a27acb9a90282
lifetime <wjqh97@gmail.com>
2023-12-04 19:30:05 +0900

    🔨 chore : newpsot margin 수정

--- src/components/common/category/NewPost.jsx
+++ src/components/common/category/NewPost.jsx
@@ -10,12 +10,12 @@ const NewPost = (props) => {
     return (
         <Container className='post-box'>
         <Link to={`/board/${props.post_id}`} className='link-style'>
-            <Row className='title mb-2' >{props.title}</Row>
-            <Row className='content'>{props.content}</Row>
-            <Row>작성자 : {props.name}</Row>
-            <Row>작성일 : {props.created_at}</Row>
+            <Row className='title mb-2' ><Col>{props.title}</Col></Row>
+            <Row className='content'><Col>{props.content}</Col></Row>
+            <Row><Col>작성자 : {props.name}</Col></Row>
+            <Row><Col>작성일 : {props.created_at}</Col></Row>
             <Row>
-                <Col className='mx-0'>{props.category}</Col>
+                <Col className=''>{props.category}</Col>
                 <Col/>
                 <Col>
                     <div><Eye className='me-2' /><span>{props.views} views</span></div>
